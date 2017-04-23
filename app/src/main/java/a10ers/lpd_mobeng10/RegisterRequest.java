817cb9eea625a922845a94deb5c65be469d01539
eliza <no.writing@gmail.com>
2017-04-24 01:23:17 +0300

    nearly logged

--- app/src/main/java/a10ers/lpd_mobeng10/RegisterRequest.java
+++ app/src/main/java/a10ers/lpd_mobeng10/RegisterRequest.java
@@ -1,8 +1,30 @@
 package a10ers.lpd_mobeng10;
 
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Eliza on 23-Apr-17.
  */
 
-public class RegisterRequest {
+public class RegisterRequest extends StringRequest {
+
+    private static final String REGISTER_REQUEST_URL = "https://show-me-the-money.000webhostapp.com/android_app/lpd_register2.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String name, String username, String password, Response.Listener<String> listener){
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("name", name);
+        params.put("username", username);
+        params.put("password", password + "");
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
 }
